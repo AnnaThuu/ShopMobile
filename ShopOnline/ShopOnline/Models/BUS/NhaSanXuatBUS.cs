69905ca097a23d3afc62280bc7c52462e587ef01
Anna Thu <doanhanhanna>
2021-05-13 16:28:32 +0700

    first commit

---
+++ ShopOnline/ShopOnline/Models/BUS/NhaSanXuatBUS.cs
@@ -0,0 +1,24 @@
+﻿using ShopOnlineDTConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class NhaSanXuatBUS
+    {
+        public static IEnumerable<NhaSanXuat> DanhSach()
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.Query<NhaSanXuat>("select * from NhaSanXuat where TinhTrang = 0");
+
+        }
+        public static IEnumerable<SanPham> ChiTiet(String id)
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where MaNhaSanXuat = '"+id+" '");
+
+        }
+    }
+}
---
+++ ShopOnline/ShopOnline/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,36 @@
+﻿using ShopOnlineDTConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 0");
+
+        }
+        public static SanPham ChiTiet(String a)
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.FirstOrDefault<SanPham>("select * from SanPham where MaSanPham = @0",a);
+
+        }
+        public static IEnumerable<SanPham> Top4New()
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.Query<SanPham>("select Top 4 * from SanPham where GhiChu = N'New'");
+
+        }
+        public static IEnumerable<SanPham> TopHot()
+        {
+            var db = new ShopOnlineDTConnectionDB();
+            return db.Query<SanPham>("select Top 4 * from SanPham where LuotView > 0");
+
+        }
+    }
+}
